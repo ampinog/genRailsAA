828fa90ce09f821d857b5848aeae4c822b93f3ed
Angel Pino <angelmauricio.pinogonzalez@gmail.com>
2018-08-27 16:03:16 -0300

    Update development_mail_interceptor.rb

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = "angelmauricio.pinogonzalez@gmail.com"
+    message.to = "tu correo_para_desarrollo@gmail.com"
   end
 end
