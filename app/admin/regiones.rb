e17d164d24946551159f6a94ed4fa5192c773ee0
Angel Pino <angelmauricio.pinogonzalez@gmail.com>
2018-10-12 00:05:46 -0300

    Create regiones.rb

---
+++ app/admin/regiones.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Region do                                     
+  decorate_with RegionDecorator                                    
+
+  permit_params :nombre, :titulo                                   
+                                                                   
+  menu label: 'Regiones', parent: 'Tablas', priority: 10000        
+
+  index do
+    column :nombre_
+    column :titulo
+  end                                                              
+  filter :nombre
+  filter :titulo
+
+  action_item :ocultar_ver_side_bar, only: :index do
+    link_to "Ocultar/Ver filtro","#", {id: 'ocultar_ver_side_bar'}
+  end
