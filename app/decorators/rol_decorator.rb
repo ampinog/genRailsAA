07316d1bdd6f70cd850e91266cc4dbc2209659e0
Angel Pino <angelmauricio.pinogonzalez@gmail.com>
2018-10-12 00:20:49 -0300

    Create rol_decorator.rb

---
+++ app/decorators/rol_decorator.rb
@@ -0,0 +1,8 @@
+class RolDecorator < Draper::Decorator
+  delegate_all
+
+  def nombre_
+    h.link_to object.nombre, action: :show, id: object.id
+  end
+end
+                                                          
